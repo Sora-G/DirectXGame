389fc60ee43ae9b76f2b55b18241fbf2971c5282
sora_suzuki <k022g0047@g.neec.ac.jp>
2024-04-16 13:19:09 +0900

    00_05の完成

--- main.cpp
+++ main.cpp
@@ -160,6 +160,31 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	assert(useAdapter != nullptr);
 
 
+	ID3D12Device* device = nullptr;
+	//機能レベルとログ出力用の文字列
+	D3D_FEATURE_LEVEL featureLevels[] =
+	{
+		D3D_FEATURE_LEVEL_12_2,D3D_FEATURE_LEVEL_12_1,D3D_FEATURE_LEVEL_12_0
+	};
+	const char* featureLevelStrings[] = { "12.2","12.1","12.0" };
+	//高い順に生成できるか試していく
+	for (size_t i = 0; i < _countof(featureLevels[i]); ++i)
+	{
+		//採用したアダプタでデバイスを生成
+		hr = D3D12CreateDevice(useAdapter, featureLevels[i], IID_PPV_ARGS(&device));
+		//指定した機能レベルでデバイスが生成できたか確認
+		if (SUCCEEDED(hr))
+		{
+			//生成できたのでログ出力を行ってループを抜ける
+			Log(std::format("FeatureLevel : {}\n", featureLevelStrings[i]));
+			break;
+		}
+	}
+
+	//デバイスの生成が上手くいかなかったので起動できない
+	assert(device != nullptr);
+	Log("Comprete Create D3D12Device!\n");
+
 	MSG msg{};
 
 	//ウィンドウの×が押されるまでループ
