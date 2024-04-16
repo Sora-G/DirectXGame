843d98595e077c11091f2c07ae233a7d5b11a346
sora_suzuki <k022g0047@g.neec.ac.jp>
2024-04-16 11:19:36 +0900

    ウィンドウプロシージャの作成

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
 #include <Windows.h>
 
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
+
+//ウィンドウプロージャ
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	//メッセージに応じてゲームの固有処理を行う
+	switch (msg)
+	{
+		//ウィンドウ破棄された
+	case WM_DESTROY:
+		//OSに対してアプリの終了を伝える
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	//標準のメッセージ処理を行う
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+
+
+
+//Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) 
+{
+
+	//出力ウィンドウへの文字出力
 	OutputDebugStringA("Hello DirectX!\n");
 
 	return 0;
