aec6ab39e6a168df22660ae4d839bf29d5fdd918
sora_suzuki <k022g0047@g.neec.ac.jp>
2024-05-14 10:59:34 +0900

    shaderをcompile

--- main.cpp
+++ main.cpp
@@ -455,6 +455,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	rasterizerDesc.FillMode = D3D12_FILL_MODE_SOLID;
 
 
+	//shaderをコンパイルする
+	IDxcBlob* vertexShaderBlob = CompileShader(L"Object3D.VS.htsl", L"vs_6_0", dxcUtils, dxcCompiler, includeHandler);
+	assert(vertexShaderBlob != nullptr);
+
+	IDxcBlob* pixelShaderBlob = CompileShader(L"Object3D.PS.htsl", L"ps_6_0", dxcUtils, dxcCompiler, includeHandler);
+	assert(pixelShaderBlob != nullptr);
+
+
 	MSG msg{};
 
 
